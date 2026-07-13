16fd132556181de39554933331d22e1f31d3428e
Kushal Das <mail@kushaldas.in>
2026-07-13 23:36:34 +0200

    add: log 0.4.33

--- stubs/log/spicycrab_log/__init__.py
+++ stubs/log/spicycrab_log/__init__.py
@@ -37,6 +37,223 @@ def eprintln(message: str) -> None:
     ...
 
 
+class Key:
+    """A key in a key-value."""
+
+    def to_key(self) -> Key: ...
+
+    @staticmethod
+    def from_str(key: object) -> "Key": ...
+
+    @staticmethod
+    def from_str_static(key: object) -> "Key": ...
+
+    def as_str(self) -> str: ...
+
+    def to_borrowed_str(self) -> object: ...
+
+    def to_static_str(self) -> object: ...
+
+    def fmt(self, f: Formatter) -> Result: ...
+
+    def as_ref(self) -> str: ...
+
+    def borrow(self) -> str: ...
+
+    @staticmethod
+    def from_(s: object) -> "Key": ...
+
+    def stream(self, stream: S) -> Result: ...
+
+    def stream_ref(self, stream: S) -> Result: ...
+
+    def serialize(self, serializer: S) -> Ok: ...
+
+class Value:
+    """A value in a key-value.
+
+Values are an anonymous bag containing some structured datum.
+
+# Capturing values
+
+There are a few ways to capture a value:
+
+- Using the `Value::from_*` methods.
+- Using the `ToValue` trait.
+- Using the standard `From` trait.
+
+## Using the `Value::from_*` methods
+
+`Value` offers a few constructor methods that capture values of different kinds.
+
+```
+use log::kv::Value;
+
+let value = Value::from_debug(&42i32);
+
+assert_eq!(None, value.to_i64());
+```
+
+## Using the `ToValue` trait
+
+The `ToValue` trait can be used to capture values generically.
+It's the bound used by `Source`.
+
+```
+# use log::kv::ToValue;
+let value = 42i32.to_value();
+
+assert_eq!(Some(42), value.to_i64());
+```
+
+## Using the standard `From` trait
+
+Standard types that implement `ToValue` also implement `From`.
+
+```
+use log::kv::Value;
+
+let value = Value::from(42i32);
+
+assert_eq!(Some(42), value.to_i64());
+```
+
+# Data model
+
+Values can hold one of a number of types:
+
+- **Null:** The absence of any other meaningful value. Note that
+`Some(Value::null())` is not the same as `None`. The former is
+`null` while the latter is `undefined`. This is important to be
+able to tell the difference between a key-value that was logged,
+but its value was empty (`Some(Value::null())`) and a key-value
+that was never logged at all (`None`).
+- **Strings:** `str`, `char`.
+- **Booleans:** `bool`.
+- **Integers:** `u8`-`u128`, `i8`-`i128`, `NonZero*`.
+- **Floating point numbers:** `f32`-`f64`.
+- **Errors:** `dyn (Error + 'static)`.
+- **`serde`:** Any type in `serde`'s data model.
+- **`sval`:** Any type in `sval`'s data model.
+
+# Serialization
+
+Values provide a number of ways to be serialized.
+
+For basic types the [`Value::visit`] method can be used to extract the
+underlying typed value. However, this is limited in the amount of types
+supported (see the [`VisitValue`] trait methods).
+
+For more complex types one of the following traits can be used:
+* `sval::Value`, requires the `kv_sval` feature.
+* `serde::Serialize`, requires the `kv_serde` feature.
+
+You don't need a visitor to serialize values through `serde` or `sval`.
+
+A value can always be serialized using any supported framework, regardless
+of how it was captured. If, for example, a value was captured using its
+`Display` implementation, it will serialize through `serde` as a string. If it was
+captured as a struct using `serde`, it will also serialize as a struct
+through `sval`, or can be formatted using a `Debug`-compatible representation."""
+
+    def to_value(self) -> Value: ...
+
+    @staticmethod
+    def from_any(value: object) -> "Value": ...
+
+    @staticmethod
+    def from_debug(value: object) -> "Value": ...
+
+    @staticmethod
+    def from_display(value: object) -> "Value": ...
+
+    @staticmethod
+    def from_serde(value: object) -> "Value": ...
+
+    @staticmethod
+    def from_sval(value: object) -> "Value": ...
+
+    @staticmethod
+    def from_dyn_debug(value: Debug) -> "Value": ...
+
+    @staticmethod
+    def from_dyn_display(value: Display) -> "Value": ...
+
+    @staticmethod
+    def from_dyn_error(err: object) -> "Value": ...
+
+    @staticmethod
+    def null() -> "Value": ...
+
+    def visit(self, visitor: object) -> None: ...
+
+    def fmt(self, f: Formatter) -> Result: ...
+
+    def fmt(self, f: Formatter) -> Result: ...
+
+    def serialize(self, s: S) -> Ok: ...
+
+    def stream(self, stream: S) -> Result: ...
+
+    def stream_ref(self, stream: S) -> Result: ...
+
+    @staticmethod
+    def from_(value: object) -> "Value": ...
+
+    def to_borrowed_error(self) -> object: ...
+
+    def to_borrowed_str(self) -> object: ...
+
+    def to_cow_str(self) -> object: ...
+
+    @staticmethod
+    def from_(v: object) -> "Value": ...
+
+    @staticmethod
+    def capture_debug(value: object) -> "Value": ...
+
+    @staticmethod
+    def capture_display(value: object) -> "Value": ...
+
+    @staticmethod
+    def capture_error(err: object) -> "Value": ...
+
+    @staticmethod
+    def capture_serde(value: object) -> "Value": ...
+
+    @staticmethod
+    def capture_sval(value: object) -> "Value": ...
+
+    def is_(self) -> bool: ...
+
+    def downcast_ref(self) -> object: ...
+
+class Error:
+    """An error encountered while working with structured data."""
+
+    @staticmethod
+    def msg(msg: object) -> "Error": ...
+
+    def fmt(self, f: Formatter) -> Result: ...
+
+    @staticmethod
+    def from_(_: Exception) -> "Error": ...
+
+    @staticmethod
+    def boxed(err: E) -> "Error": ...
+
+    @staticmethod
+    def from_(err: Exception) -> "Error": ...
+
+class GlobalLogger:
+    """The global logger proxy."""
+
+    def enabled(self, metadata: Metadata) -> bool: ...
+
+    def log(self, record: Record) -> None: ...
+
+    def flush(self) -> None: ...
+
 class Record:
     """The "payload" of a log message.
 
@@ -269,303 +486,6 @@ class ParseLevelError:
 
     def fmt(self, fmt: Formatter) -> Result: ...
 
-class Key:
-    """A key in a key-value."""
-
-    def to_key(self) -> Key: ...
-
-    @staticmethod
-    def from_str(key: object) -> "Key": ...
-
-    def as_str(self) -> str: ...
-
-    def to_borrowed_str(self) -> object: ...
-
-    def fmt(self, f: Formatter) -> Result: ...
-
-    def as_ref(self) -> str: ...
-
-    def borrow(self) -> str: ...
-
-    @staticmethod
-    def from_(s: object) -> "Key": ...
-
-    def stream(self, stream: S) -> Result: ...
-
-    def stream_ref(self, stream: S) -> Result: ...
-
-    def serialize(self, serializer: S) -> Ok: ...
-
-class Value:
-    """A value in a key-value.
-
-Values are an anonymous bag containing some structured datum.
-
-# Capturing values
-
-There are a few ways to capture a value:
-
-- Using the `Value::from_*` methods.
-- Using the `ToValue` trait.
-- Using the standard `From` trait.
-
-## Using the `Value::from_*` methods
-
-`Value` offers a few constructor methods that capture values of different kinds.
-
-```
-use log::kv::Value;
-
-let value = Value::from_debug(&42i32);
-
-assert_eq!(None, value.to_i64());
-```
-
-## Using the `ToValue` trait
-
-The `ToValue` trait can be used to capture values generically.
-It's the bound used by `Source`.
-
-```
-# use log::kv::ToValue;
-let value = 42i32.to_value();
-
-assert_eq!(Some(42), value.to_i64());
-```
-
-## Using the standard `From` trait
-
-Standard types that implement `ToValue` also implement `From`.
-
-```
-use log::kv::Value;
-
-let value = Value::from(42i32);
-
-assert_eq!(Some(42), value.to_i64());
-```
-
-# Data model
-
-Values can hold one of a number of types:
-
-- **Null:** The absence of any other meaningful value. Note that
-`Some(Value::null())` is not the same as `None`. The former is
-`null` while the latter is `undefined`. This is important to be
-able to tell the difference between a key-value that was logged,
-but its value was empty (`Some(Value::null())`) and a key-value
-that was never logged at all (`None`).
-- **Strings:** `str`, `char`.
-- **Booleans:** `bool`.
-- **Integers:** `u8`-`u128`, `i8`-`i128`, `NonZero*`.
-- **Floating point numbers:** `f32`-`f64`.
-- **Errors:** `dyn (Error + 'static)`.
-- **`serde`:** Any type in `serde`'s data model.
-- **`sval`:** Any type in `sval`'s data model.
-
-# Serialization
-
-Values provide a number of ways to be serialized.
-
-For basic types the [`Value::visit`] method can be used to extract the
-underlying typed value. However, this is limited in the amount of types
-supported (see the [`VisitValue`] trait methods).
-
-For more complex types one of the following traits can be used:
-* `sval::Value`, requires the `kv_sval` feature.
-* `serde::Serialize`, requires the `kv_serde` feature.
-
-You don't need a visitor to serialize values through `serde` or `sval`.
-
-A value can always be serialized using any supported framework, regardless
-of how it was captured. If, for example, a value was captured using its
-`Display` implementation, it will serialize through `serde` as a string. If it was
-captured as a struct using `serde`, it will also serialize as a struct
-through `sval`, or can be formatted using a `Debug`-compatible representation."""
-
-    def to_value(self) -> Value: ...
-
-    @staticmethod
-    def from_any(value: object) -> "Value": ...
-
-    @staticmethod
-    def from_debug(value: object) -> "Value": ...
-
-    @staticmethod
-    def from_display(value: object) -> "Value": ...
-
-    @staticmethod
-    def from_serde(value: object) -> "Value": ...
-
-    @staticmethod
-    def from_sval(value: object) -> "Value": ...
-
-    @staticmethod
-    def from_dyn_debug(value: Debug) -> "Value": ...
-
-    @staticmethod
-    def from_dyn_display(value: Display) -> "Value": ...
-
-    @staticmethod
-    def from_dyn_error(err: object) -> "Value": ...
-
-    @staticmethod
-    def null() -> "Value": ...
-
-    def visit(self, visitor: object) -> None: ...
-
-    def fmt(self, f: Formatter) -> Result: ...
-
-    def fmt(self, f: Formatter) -> Result: ...
-
-    def serialize(self, s: S) -> Ok: ...
-
-    def stream(self, stream: S) -> Result: ...
-
-    def stream_ref(self, stream: S) -> Result: ...
-
-    @staticmethod
-    def from_(value: object) -> "Value": ...
-
-    def to_borrowed_error(self) -> object: ...
-
-    def to_borrowed_str(self) -> object: ...
-
-    def to_cow_str(self) -> object: ...
-
-    @staticmethod
-    def from_(v: object) -> "Value": ...
-
-    @staticmethod
-    def capture_debug(value: object) -> "Value": ...
-
-    @staticmethod
-    def capture_display(value: object) -> "Value": ...
-
-    @staticmethod
-    def capture_error(err: object) -> "Value": ...
-
-    @staticmethod
-    def capture_serde(value: object) -> "Value": ...
-
-    @staticmethod
-    def capture_sval(value: object) -> "Value": ...
-
-    def is_(self) -> bool: ...
-
-    def downcast_ref(self) -> object: ...
-
-class Error:
-    """An error encountered while working with structured data."""
-
-    @staticmethod
-    def msg(msg: object) -> "Error": ...
-
-    def fmt(self, f: Formatter) -> Result: ...
-
-    @staticmethod
-    def from_(_: Exception) -> "Error": ...
-
-    @staticmethod
-    def boxed(err: E) -> "Error": ...
-
-    @staticmethod
-    def from_(err: Exception) -> "Error": ...
-
-class GlobalLogger:
-    """The global logger proxy."""
-
-    def enabled(self, metadata: Metadata) -> bool: ...
-
-    def log(self, record: Record) -> None: ...
-
-    def flush(self) -> None: ...
-
-class Level:
-    """An enum representing the available verbosity levels of the logger.
-
-Typical usage includes: checking if a certain `Level` is enabled with
-[`log_enabled!`](macro.log_enabled.html), specifying the `Level` of
-[`log!`](macro.log.html), and comparing a `Level` directly to a
-[`LevelFilter`](enum.LevelFilter.html)."""
-    Error: "Level"
-    Warn: "Level"
-    Info: "Level"
-    Debug: "Level"
-    Trace: "Level"
-
-    def serialize(self, serializer: S) -> Ok: ...
-
-    @staticmethod
-    def deserialize(deserializer: D) -> object: ...
-
-    def eq(self, other: LevelFilter) -> bool: ...
-
-    def partial_cmp(self, other: LevelFilter) -> Ordering | None: ...
-
-    @staticmethod
-    def from_str(level: str) -> "Level": ...
-
-    def fmt(self, fmt: Formatter) -> Result: ...
-
-    @staticmethod
-    def max() -> "Level": ...
-
-    def to_level_filter(self) -> LevelFilter: ...
-
-    def as_str(self) -> object: ...
-
-    @staticmethod
-    def iter() -> object: ...
-
-    def increment_severity(self) -> Self: ...
-
-    def decrement_severity(self) -> Self: ...
-
-class LevelFilter:
-    """An enum representing the available verbosity level filters of the logger.
-
-A `LevelFilter` may be compared directly to a [`Level`]. Use this type
-to get and set the maximum log level with [`max_level()`] and [`set_max_level`].
-
-[`Level`]: enum.Level.html
-[`max_level()`]: fn.max_level.html
-[`set_max_level`]: fn.set_max_level.html"""
-    Off: "LevelFilter"
-    Error: "LevelFilter"
-    Warn: "LevelFilter"
-    Info: "LevelFilter"
-    Debug: "LevelFilter"
-    Trace: "LevelFilter"
-
-    def serialize(self, serializer: S) -> Ok: ...
-
-    @staticmethod
-    def deserialize(deserializer: D) -> object: ...
-
-    def eq(self, other: Level) -> bool: ...
-
-    def partial_cmp(self, other: Level) -> Ordering | None: ...
-
-    @staticmethod
-    def from_str(level: str) -> "LevelFilter": ...
-
-    def fmt(self, fmt: Formatter) -> Result: ...
-
-    @staticmethod
-    def max() -> "LevelFilter": ...
-
-    def to_level(self) -> Level | None: ...
-
-    def as_str(self) -> object: ...
-
-    @staticmethod
-    def iter() -> object: ...
-
-    def increment_severity(self) -> Self: ...
-
-    def decrement_severity(self) -> Self: ...
-
 class Inner:
     None_: "Inner"
     Bool: "Inner"
@@ -678,6 +598,113 @@ class Token:
     F64: "Token"
     I64: "Token"
     U64: "Token"
+
+class Level:
+    """An enum representing the available verbosity levels of the logger.
+
+Typical usage includes: checking if a certain `Level` is enabled with
+[`log_enabled!`](macro.log_enabled.html), specifying the `Level` of
+[`log!`](macro.log.html), and comparing a `Level` directly to a
+[`LevelFilter`](enum.LevelFilter.html)."""
+    Error: "Level"
+    Warn: "Level"
+    Info: "Level"
+    Debug: "Level"
+    Trace: "Level"
+
+    def serialize(self, serializer: S) -> Ok: ...
+
+    @staticmethod
+    def deserialize(deserializer: D) -> object: ...
+
+    def eq(self, other: LevelFilter) -> bool: ...
+
+    def partial_cmp(self, other: LevelFilter) -> Ordering | None: ...
+
+    @staticmethod
+    def from_str(level: str) -> "Level": ...
+
+    def fmt(self, fmt: Formatter) -> Result: ...
+
+    @staticmethod
+    def max() -> "Level": ...
+
+    def to_level_filter(self) -> LevelFilter: ...
+
+    def as_str(self) -> object: ...
+
+    @staticmethod
+    def iter() -> object: ...
+
+    def increment_severity(self) -> Self: ...
+
+    def decrement_severity(self) -> Self: ...
+
+class LevelFilter:
+    """An enum representing the available verbosity level filters of the logger.
+
+A `LevelFilter` may be compared directly to a [`Level`]. Use this type
+to get and set the maximum log level with [`max_level()`] and [`set_max_level`].
+
+[`Level`]: enum.Level.html
+[`max_level()`]: fn.max_level.html
+[`set_max_level`]: fn.set_max_level.html"""
+    Off: "LevelFilter"
+    Error: "LevelFilter"
+    Warn: "LevelFilter"
+    Info: "LevelFilter"
+    Debug: "LevelFilter"
+    Trace: "LevelFilter"
+
+    def serialize(self, serializer: S) -> Ok: ...
+
+    @staticmethod
+    def deserialize(deserializer: D) -> object: ...
+
+    def eq(self, other: Level) -> bool: ...
+
+    def partial_cmp(self, other: Level) -> Ordering | None: ...
+
+    @staticmethod
+    def from_str(level: str) -> "LevelFilter": ...
+
+    def fmt(self, fmt: Formatter) -> Result: ...
+
+    @staticmethod
+    def max() -> "LevelFilter": ...
+
+    def to_level(self) -> Level | None: ...
+
+    def as_str(self) -> object: ...
+
+    @staticmethod
+    def iter() -> object: ...
+
+    def increment_severity(self) -> Self: ...
+
+    def decrement_severity(self) -> Self: ...
+
+def visit(inner: Inner, visitor: object) -> None: ...
+
+def visit(inner: Inner, visitor: object) -> None: ...
+
+def log(logger: L, args: Arguments, level: Level, target_module_path_and_loc: object, kvs: K) -> None: ...
+
+def enabled(logger: L, level: Level, target: str) -> bool: ...
+
+def loc() -> object: ...
+
+def capture_to_value(v: object) -> Value: ...
+
+def capture_debug(v: object) -> Value: ...
+
+def capture_display(v: object) -> Value: ...
+
+def capture_error(v: object) -> Value: ...
+
+def capture_sval(v: object) -> Value: ...
+
+def capture_serde(v: object) -> Value: ...
 
 """Sets the global maximum log level.
 
@@ -818,26 +845,4 @@ def set_logger_racy(logger: object) -> None: ...
 If a logger has not been set, a no-op implementation is returned."""
 def logger() -> object: ...
 
-def visit(inner: Inner, visitor: object) -> None: ...
-
-def visit(inner: Inner, visitor: object) -> None: ...
-
-def log(logger: L, args: Arguments, level: Level, target_module_path_and_loc: object, kvs: K) -> None: ...
-
-def enabled(logger: L, level: Level, target: str) -> bool: ...
-
-def loc() -> object: ...
-
-def capture_to_value(v: object) -> Value: ...
-
-def capture_debug(v: object) -> Value: ...
-
-def capture_display(v: object) -> Value: ...
-
-def capture_error(v: object) -> Value: ...
-
-def capture_sval(v: object) -> Value: ...
-
-def capture_serde(v: object) -> Value: ...
-
-__all__: list[str] = ["set_max_level", "set_max_level_racy", "max_level", "set_boxed_logger", "set_logger", "set_logger_racy", "logger", "visit", "visit", "log", "enabled", "loc", "capture_to_value", "capture_debug", "capture_display", "capture_error", "capture_sval", "capture_serde", "Record", "RecordBuilder", "Metadata", "MetadataBuilder", "SetLoggerError", "ParseLevelError", "Key", "Value", "Error", "GlobalLogger", "Level", "LevelFilter", "Inner", "Token"]
+__all__: list[str] = ["visit", "visit", "log", "enabled", "loc", "capture_to_value", "capture_debug", "capture_display", "capture_error", "capture_sval", "capture_serde", "set_max_level", "set_max_level_racy", "max_level", "set_boxed_logger", "set_logger", "set_logger_racy", "logger", "Key", "Value", "Error", "GlobalLogger", "Record", "RecordBuilder", "Metadata", "MetadataBuilder", "SetLoggerError", "ParseLevelError", "Inner", "Token", "Level", "LevelFilter"]
